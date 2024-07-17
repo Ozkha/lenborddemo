810308fa78e7534bc88da889272ae0c9fbc7eff4
Ozkha <karinozkargv@gmail.com>
2024-07-17 00:22:08 -0600

    boards page worker and boardmoderator permissions seted

--- src/app/app/boards/page.tsx
+++ src/app/app/boards/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import BoardsPage from "./clientpage";
 import { auth } from "@/lib/auth";
 import { db as database } from "@/db";
-import { boards } from "@/db/schema";
+import { boards, userBoardResponsabiliy, users } from "@/db/schema";
 import { eq, sql } from "drizzle-orm";
 
 export default async function BoardsPageS() {
@@ -15,13 +15,41 @@ export default async function BoardsPageS() {
   } else {
     redirect("/login");
   }
-
   const user = session.user;
   const db = await database;
-  const boardList = await db
-    .select()
-    .from(boards)
-    .where(sql`${boards.companyId}=${user.companyId}`);
+
+  const [userInfo] = await db
+    .select({
+      id: users.id,
+      username: users.id,
+      role: users.role,
+      status: users.status,
+    })
+    .from(users)
+    .where(sql`${users.id}=${user.id}`);
+
+  if (userInfo.role == "worker") {
+    redirect("/app/tasks?user=" + userInfo.id);
+  }
+
+  const sqlWhereQuery = sql`${boards.companyId}=${user.companyId}`;
+
+  const userModeratorBoardResps = await db
+    .select({ boardId: userBoardResponsabiliy.boardId })
+    .from(userBoardResponsabiliy)
+    .where(sql`${userBoardResponsabiliy.userId}=${userInfo.id}`);
+
+  if (userInfo.role == "board_moderator") {
+    userModeratorBoardResps.map(({ boardId }, index) => {
+      if (index == 0) {
+        sqlWhereQuery.append(sql`and ${boards.id}=${boardId}`);
+      } else {
+        sqlWhereQuery.append(sql`or ${boards.id}=${boardId}`);
+      }
+    });
+  }
+
+  const boardList = await db.select().from(boards).where(sqlWhereQuery);
 
   return (
     <>
