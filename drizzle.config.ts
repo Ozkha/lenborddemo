50327a681af60d6e724eb7b79751c16da8459d2a
Ozkha <karinozkargv@gmail.com>
2024-08-23 23:25:56 -0600

    feat & fix: drizzle kit for migrations with usage of .env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env" });
+
 export default defineConfig({
   //@ts-ignore
   dialect: "mysql", // "mysql" | "sqlite" | "postgresql"
   schema: "src/db/schema.ts",
   out: "src/db/migrations",
+  dbCredentials: {
+    // @ts-ignore
+    url: process.env.DATABASE_URL!,
+  },
 });
--- src/db/migrate.ts
+++
@@ -1,7 +0,0 @@
-import { migrate } from "drizzle-orm/mysql2/migrator";
-import { db, connection } from "./index";
-
-const migration = (async () => {
-  await migrate(await db, { migrationsFolder: "src/db/migrations" });
-  await (await connection).end();
-})();
