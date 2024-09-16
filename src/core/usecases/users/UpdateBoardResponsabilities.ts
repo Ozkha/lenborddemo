b4a642f059a28535f6520e8952be9f08dcb3c7ee
Ozkha <karinozkargv@gmail.com>
2024-09-16 00:02:10 -0600

    fix: UpdateBoardResponsabilties delete all responsabilities before assing new responsabilities, if the resonsabilities are 0, there will just delete all already if exits board responsabilities

--- src/core/usecases/users/UpdateBoardResponsabilities.ts
+++ src/core/usecases/users/UpdateBoardResponsabilities.ts
@@ -1,4 +1,8 @@
-import { UserRepository } from "@/core/repositories/UserRepository";
+import {
+  Role,
+  Status,
+  UserRepository,
+} from "@/core/repositories/UserRepository";
 
 type UpdateBoardResponsabilitiesProps = {
   id: number;
@@ -12,11 +16,48 @@ export function UpdateBoardResponsabilitiesWrapper(
     id,
     boardResponsabilitiesToSet,
   }: UpdateBoardResponsabilitiesProps) {
-    const userUpdatedBoardResps = await userRepository.setBoardResponsibilities(
-      id,
-      boardResponsabilitiesToSet
-    );
+    // TODO: agregar la userRepositori, deleteAllBoardResponsabilities.
+    // y que se ejecute aqui.
 
-    return userUpdatedBoardResps;
+    await userRepository.deleteAllBoardResponsibilities(id);
+
+    let userUpdatedBoardResps: {
+      id: number;
+      name: string;
+      username: string;
+      role: Role;
+      status: Status;
+      companyId: number;
+      boardsResponsibilities: {
+        id: number;
+        boardId: number;
+        name: string;
+      }[];
+    };
+
+    if (boardResponsabilitiesToSet.length > 0) {
+      userUpdatedBoardResps = await userRepository.setBoardResponsibilities(
+        id,
+        boardResponsabilitiesToSet
+      );
+    } else {
+      const thisUser = await userRepository.getById(id);
+      if (thisUser) {
+        userUpdatedBoardResps = {
+          id: thisUser.id,
+          username: thisUser.username,
+          name: thisUser.name,
+          role: thisUser.role,
+          status: thisUser.status,
+          companyId: thisUser.companyId,
+          boardsResponsibilities: [],
+        };
+      }
+    }
+
+    // @ts-expect-error I think it will cause an error if this does not work and it will not ever will be undefined or an empty variable.
+    const updatedUser = userUpdatedBoardResps;
+
+    return updatedUser;
   };
 }
