1a2657fd3797cd3ff669aa0484dd076158eb309e
Ozkha <karinozkargv@gmail.com>
2024-09-15 23:12:37 -0600

    refactor: delete unused imports of getUser api route

--- src/app/api/getuser/route.ts
+++ src/app/api/getuser/route.ts
@@ -1,7 +1,5 @@
 import { UserRepository } from "@/core/repositories/UserRepository";
 import { db } from "@/db";
-import { users } from "@/db/schema";
-import { sql } from "drizzle-orm";
 
 // TODO: Hacer mas seguro este endpoint, que no seleccione el usuario, sino que se encargue directamente de verificar
 // si esta correcto o no lo ingresado, y despues regresara solo la informaion necesaria.
